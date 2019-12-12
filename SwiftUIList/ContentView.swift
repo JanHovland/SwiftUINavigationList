b4744471638fd625d3f37902419dbe4db3049099
Jan Hovland <jan.hovland@lyse.net>
2019-12-12 11:59:04 +0100

    Update

--- SwiftUIList/ContentView.swift
+++ SwiftUIList/ContentView.swift
@@ -11,26 +11,26 @@ import SwiftUI
 struct ContentView: View {
     
     @State var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
-                   Restaurant(name: "Homei", image: "homei"),
-                   Restaurant(name: "Teakha", image: "teakha"),
-                   Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
-                   Restaurant(name: "Petite Oyster", image: "petiteoyster"),
-                   Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
-                   Restaurant(name: "Po's Atelier", image: "posatelier"),
-                   Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
-                   Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
-                   Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
-                   Restaurant(name: "Homei", image: "upstate"),
-                   Restaurant(name: "Traif", image: "traif"),
-                   Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
-                   Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
-                   Restaurant(name: "Five Leaves", image: "fiveleaves"),
-                   Restaurant(name: "Cafe Lore", image: "cafelore"),
-                   Restaurant(name: "Confessional", image: "confessional"),
-                   Restaurant(name: "Barrafina", image: "barrafina"),
-                   Restaurant(name: "Donostia", image: "donostia"),
-                   Restaurant(name: "Royal Oak", image: "royaloak"),
-                   Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+                               Restaurant(name: "Homei", image: "homei"),
+                               Restaurant(name: "Teakha", image: "teakha"),
+                               Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+                               Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+                               Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+                               Restaurant(name: "Po's Atelier", image: "posatelier"),
+                               Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+                               Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+                               Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+                               Restaurant(name: "Homei", image: "upstate"),
+                               Restaurant(name: "Traif", image: "traif"),
+                               Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+                               Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+                               Restaurant(name: "Five Leaves", image: "fiveleaves"),
+                               Restaurant(name: "Cafe Lore", image: "cafelore"),
+                               Restaurant(name: "Confessional", image: "confessional"),
+                               Restaurant(name: "Barrafina", image: "barrafina"),
+                               Restaurant(name: "Donostia", image: "donostia"),
+                               Restaurant(name: "Royal Oak", image: "royaloak"),
+                               Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
     ]
     
     var body: some View {
@@ -40,6 +40,7 @@ struct ContentView: View {
                     .contextMenu {
                         Button(action: {
                             // delete the selected restasurant
+                            self.delete(item: restaurant)
                         }) {
                             HStack {
                                 Text("Delete")
@@ -61,8 +62,15 @@ struct ContentView: View {
             }
         }
 
-
     }
+
+    private func delete(item restaurant: Restaurant) {
+        if let index = self.restaurants.firstIndex(where: { $0.id == restaurant.id} ) {
+            self.restaurants.remove(at: index)
+        }
+    }
+
+
 }
 
 struct ContentView_Previews: PreviewProvider {
